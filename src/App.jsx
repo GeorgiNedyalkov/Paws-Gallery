00330f8040327806f6ec26cb998cc2f19ec9d547
Flu3nt <gnedyalkov94@gmail.com>
2023-06-26 19:18:09 +0300

    feature: fetch 50 random dogs image from api

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,25 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
+
+const apiURL = "https://dog.ceo/api/breeds/image/random/50";
 
 function App() {
+  const [dogs, setDogs] = useState([]);
+
+  useEffect(() => {
+    fetch(apiURL)
+      .then((res) => res.json())
+      .then((data) => setDogs(data.message));
+  }, []);
+
   return (
     <>
       <header>
         <h1 className="text-3xl font-bold text-center h-16">Dogs Gallery</h1>
+        {dogs.map((dogImage) => (
+          <div>
+            <img src={dogImage} alt="" />
+          </div>
+        ))}
       </header>
     </>
   );
