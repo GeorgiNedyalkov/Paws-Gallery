ff4a0611519dedee332e53a43b965f014b0f9a2b
Flu3nt <gnedyalkov94@gmail.com>
2023-06-26 19:41:29 +0300

    feature: create dogs list and dogs components

--- src/App.jsx
+++ src/App.jsx
@@ -12,17 +12,39 @@ function App() {
   }, []);
 
   return (
-    <>
+    <div className="mx-auto w-11/12 bg-slate-100">
       <header>
         <h1 className="text-3xl font-bold text-center h-16">Dogs Gallery</h1>
-        {dogs.map((dogImage) => (
-          <div>
-            <img src={dogImage} alt="" />
-          </div>
-        ))}
       </header>
-    </>
+      <main>
+        <DogsList dogs={dogs} />
+      </main>
+    </div>
   );
 }
 
 export default App;
+
+const DogsList = ({ dogs }) => {
+  return (
+    <ul className="grid grid-cols-1 gap-5 sm:grid-cols-2  lg:grid-cols-3">
+      {dogs.map((dog, idx) => (
+        <li key={dog + idx}>
+          <Dog dog={dog} />
+        </li>
+      ))}
+    </ul>
+  );
+};
+
+const Dog = ({ dog }) => {
+  return (
+    <div className="w-64 h-64">
+      <img
+        src={dog}
+        className="object-cover w-full h-full rounded shadow-md hover:scale-125"
+        alt=""
+      />
+    </div>
+  );
+};
