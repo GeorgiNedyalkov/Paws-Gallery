0e32a92606441c0ce46238556b85b4521da8d36d
Flu3nt <gnedyalkov94@gmail.com>
2023-06-26 19:09:20 +0300

    Set up app and install tailwind

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import { useState } from "react";
+
+function App() {
+  return (
+    <>
+      <header>
+        <h1 className="text-3xl font-bold text-center h-16">Dogs Gallery</h1>
+      </header>
+    </>
+  );
+}
+
+export default App;
