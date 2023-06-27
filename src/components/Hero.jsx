ab15e547550c9ba6c0845dc8dcbe725692992801
Flu3nt <gnedyalkov94@gmail.com>
2023-06-27 11:35:06 +0300

    bugfix: responsive design on medium devices now does not hide hero under doglist

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,10 +1,10 @@
 export default function Hero() {
   return (
-    <div className="sm:h-12 lg:h-64 flex flex-col lg:items-center lg:justify-center">
+    <div className="h-32 lg:h-64 flex flex-col lg:items-center lg:justify-center">
       <h2 className="text-lg lg:text-xl mb-5">
         Welcome to the Paws gallery! 🐾
       </h2>
-      <p className="mb-5 text-center">
+      <p className="mb-5 lg:text-center">
         You can browse through our cute dogs and click on a dog to see its full
         image.
       </p>
