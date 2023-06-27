9e8232c21f40bedddf2fd35e30d466790d1ecfb9
Flu3nt <gnedyalkov94@gmail.com>
2023-06-27 11:36:44 +0300

    styling: increase font weigth of paws gallery text in hero

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -2,7 +2,7 @@ export default function Hero() {
   return (
     <div className="h-32 lg:h-64 flex flex-col lg:items-center lg:justify-center">
       <h2 className="text-lg lg:text-xl mb-5">
-        Welcome to the Paws gallery! 🐾
+        Welcome to the <b>Paws gallery</b>! 🐾
       </h2>
       <p className="mb-5 lg:text-center">
         You can browse through our cute dogs and click on a dog to see its full
